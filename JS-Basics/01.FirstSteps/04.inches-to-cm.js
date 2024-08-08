8f8a0e22bfbf2521b454a40cb2fe5e8361a5d402
oksanakney <oksanakney@gmail.com>
2024-08-08 21:58:41 +0300

    JS-Basics - first exercises Added

---
+++ JS-Basics/01.FirstSteps/04.inches-to-cm.js
@@ -0,0 +1,8 @@
+function convertInchesToCms(input) {
+    let inches = Number(input[0]);
+    let cms = inches * 2.54;
+    console.log(cms);
+}
+
+convertInchesToCms(["5"]);
+convertInchesToCms(["7"]);
