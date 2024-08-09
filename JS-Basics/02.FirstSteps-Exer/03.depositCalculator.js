e233beca9202308cbffca5b8fecde3a1f17ce4ee
oksanakney <oksanakney@gmail.com>
2024-08-09 21:29:33 +0300

    Update 03.depositCalculator.js

--- JS-Basics/02.FirstSteps-Exer/03.depositCalculator.js
+++ JS-Basics/02.FirstSteps-Exer/03.depositCalculator.js
@@ -2,18 +2,9 @@ function calculateDeposit(input) {
     let amountDeposited = Number(input[0]);
     let terms = Number(input[1]);
     let annualInterestRate = Number(input[2]);
-
-    /*сума = депозирана сума  + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
-Вход
-От конзолата се четат 3 реда:
-    1. Депозирана сума – реално число в интервала [100.00 … 10000.00]
-    2. Срок на депозита (в месеци) – цяло число в интервала [1…12]
-    3. Годишен лихвен процент – реално число в интервала [0.00 …100.00] */
-
-    let sum = amountDeposited + terms * ((amountDeposited * annualInterestRate/100) / 12);
+    let sum = amountDeposited + terms * ((amountDeposited * annualInterestRate / 100) / 12);
 
     console.log(sum.toFixed(2));
-
 }
 
 calculateDeposit(["200 ", "3 ", "5.7 "]);
