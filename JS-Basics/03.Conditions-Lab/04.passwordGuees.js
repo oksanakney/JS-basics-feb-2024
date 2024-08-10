6f82beb00d35b8dfd7ad109b2f1782af4f120fde
oksanakney <oksanakney@gmail.com>
2024-08-10 15:57:52 +0300

    Create 04.passwordGuees.js

---
+++ JS-Basics/03.Conditions-Lab/04.passwordGuees.js
@@ -0,0 +1,13 @@
+function guessPassword(input) {
+    let password = input[0];
+
+    if (password === "s3cr3t!P@ssw0rd") {
+        console.log("Welcome");
+    } else {
+        console.log("Wrong password!");        
+    }
+}
+
+guessPassword(["s3cr3t!P@ssw0rd"]);
+guessPassword(["jkfashdfihsdfiphep"]);
+guessPassword(["3928rihf98384fnHUG##8"]);
