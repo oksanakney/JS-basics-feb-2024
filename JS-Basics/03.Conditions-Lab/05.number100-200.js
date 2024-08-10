bdfc7cb242294363f6bf3fc127b5c8553e06bd65
oksanakney <oksanakney@gmail.com>
2024-08-10 15:57:54 +0300

    Create 05.number100-200.js

---
+++ JS-Basics/03.Conditions-Lab/05.number100-200.js
@@ -0,0 +1,16 @@
+function checkNumber(input) {
+    let number = Number(input[0]);
+
+    if (number >= 100 && number <= 200) {
+        console.log("Between 100 and 200");        
+    } else if (number < 100) {
+        console.log("Less than 100");        
+    } else {
+        console.log("Greater than 200");        
+    }
+}
+
+checkNumber(["95"]);
+checkNumber(["120"]);
+checkNumber(["210"]);
+checkNumber(["-50"]);
