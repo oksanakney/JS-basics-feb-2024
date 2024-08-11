fd9358b5b02b34eb8cb88e6340f121b95e45bc54
oksanakney <oksanakney@gmail.com>
2024-08-11 21:07:21 +0300

    Create 02.WeekendOrWorkDay.js

---
+++ JS-Basics/05.Conditions2-Lab/02.WeekendOrWorkDay.js
@@ -0,0 +1,26 @@
+function printResult(input) {
+    let day = input[0];
+
+    switch (day) {
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+            console.log("Working day");            
+            break;
+        case "Saturday":
+        case "Sunday":
+            console.log("Weekend");
+            break;
+               
+        default:
+            console.log("Error");            
+            break;
+    }
+}
+
+printResult(["Monday"]);
+printResult(["Sunday"]);
+printResult(["April"]);
+printResult(["Friday"]);
