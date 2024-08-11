cfa63e9978e6f3bafc8f1e5ae4e9f997ebe8db0d
oksanakney <oksanakney@gmail.com>
2024-08-11 22:07:05 +0300

    Update 15.fruitShopIf.js

--- JS-Basics/05.Conditions2-Lab/15.fruitShopIf.js
+++ JS-Basics/05.Conditions2-Lab/15.fruitShopIf.js
@@ -2,52 +2,51 @@ function calculatePrice(input) {
     let fruit = input[0];
     let day = input[1];
     let quantity = Number(input[2]);
-
-    let price = 0;
-    let errorMessage = "error";
+    let price = 0;  
     let result = 0;
-    
-    if (day === "Monday" || day === "Tuesday" || day === "Wednesday" || day === "Thursday" || day === "Friday") {
-        if (fruit === "banana") {
-            price = 2.5;
-        } else if (fruit === "apple") {
-            price = 1.2;
-        } else if (fruit === "orange") {
-            price = 0.85;
-        } else if (fruit === "grapefruit") {
-            price = 1.45;
-        } else if (fruit === "kiwi") {
-            price = 2.7;
-        } else if (fruit === "pineapple") {
-            price = 5.5;
-        } else if (fruit === "grapes") {
-            price = 3.85;                        
-        }       
-    } else if (day === "Saturday" || day === "Sunday") {
-        if (fruit === "banana") {
-            price = 2.7;
-        } else if (fruit === "apple") {
-            price = 1.25;
-        } else if (fruit === "orange") {
-            price = 0.9;
-        } else if (fruit === "grapefruit") {
-            price = 1.6;
-        } else if (fruit === "kiwi") {
-            price = 3;
-        } else if (fruit === "pineapple") {
-            price = 5.6;
-        } else if (fruit === "grapes") {
-            price = 4.2;
-        }
-    }
 
-    result = price * quantity;
-    
-    if (day !== "Monday" || day !== "Tuesday" || day!== "Wednesday" || day !== "Thursday" || day !== "Friday" || day !== "Saturday" || day !== "Sunday" || fruit !== "banana" || fruit !== "apple" || fruit !== "orange" || fruit !== "grapefruit" || fruit !== "kiwi" || fruit !== "pineapple" || fruit !== "grapes") {
-        console.log("error");        
+    let isValidDay = day === "Monday" || day === "Tuesday" || day=== "Wednesday" || day === "Thursday" || day === "Friday" || day === "Saturday" || day === "Sunday";
+    let isValidFood = fruit === "banana" || fruit === "apple" || fruit === "orange" || fruit === "grapefruit" || fruit === "kiwi" || fruit === "pineapple" || fruit === "grapes";
+    if (!isValidDay || !isValidFood) {
+        console.log("error");
     } else {
-        console.log(result.toFixed);        
-    }
+        if (day === "Monday" || day === "Tuesday" || day === "Wednesday" || day === "Thursday" || day === "Friday") {
+            if (fruit === "banana") {
+                price = 2.5;
+            } else if (fruit === "apple") {
+                price = 1.2;
+            } else if (fruit === "orange") {
+                price = 0.85;
+            } else if (fruit === "grapefruit") {
+                price = 1.45;
+            } else if (fruit === "kiwi") {
+                price = 2.7;
+            } else if (fruit === "pineapple") {
+                price = 5.5;
+            } else if (fruit === "grapes") {
+                price = 3.85;                        
+            }       
+        } else if (day === "Saturday" || day === "Sunday") {
+            if (fruit === "banana") {
+                price = 2.7;
+            } else if (fruit === "apple") {
+                price = 1.25;
+            } else if (fruit === "orange") {
+                price = 0.9;
+            } else if (fruit === "grapefruit") {
+                price = 1.6;
+            } else if (fruit === "kiwi") {
+                price = 3;
+            } else if (fruit === "pineapple") {
+                price = 5.6;
+            } else if (fruit === "grapes") {
+                price = 4.2;
+            }
+        }
+    
+        result = price * quantity;   
+        console.log(result.toFixed(2)); 
+    }      
 }
 
 calculatePrice(["apple", "Tuesday", "2"]);
