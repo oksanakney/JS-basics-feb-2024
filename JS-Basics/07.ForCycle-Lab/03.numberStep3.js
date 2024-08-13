c55711f756111bbba4906eb186dd787de86c0542
oksanakney <oksanakney@gmail.com>
2024-08-13 20:51:22 +0300

    Create 03.numberStep3.js

---
+++ JS-Basics/07.ForCycle-Lab/03.numberStep3.js
@@ -0,0 +1,11 @@
+function printNumbers(input) {
+    let n = Number(input[0]);
+    for (i = 1; i <= n; i+=3) {
+        console.log(i);
+        
+    }
+}
+
+printNumbers(["10"]);
+printNumbers(["7"]);
+printNumbers(["15"]);
