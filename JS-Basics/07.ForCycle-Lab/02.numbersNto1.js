aa6d730ff8f0a64a941da18a5e54c35c909b1c0c
oksanakney <oksanakney@gmail.com>
2024-08-13 20:51:18 +0300

    Create 02.numbersNto1.js

---
+++ JS-Basics/07.ForCycle-Lab/02.numbersNto1.js
@@ -0,0 +1,12 @@
+function printNumbers(input) {
+    let number = Number(input[0]);
+
+    for (let i = number; i > 0; i-- ) {
+        console.log(i);
+        
+    }
+}
+
+printNumbers(["3"]);
+printNumbers(["5"]);
+printNumbers(["6"]);
